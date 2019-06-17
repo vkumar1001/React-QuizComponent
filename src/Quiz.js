47d135c11a43189eee24b5ab12706ff06bebe9bb
vinay kumar <vkumarnit08@gmail.com>
2019-06-17 17:27:24 +0530

    data binding among components

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,13 +8,17 @@ class Quiz extends Component{
         super(props);
         this.state = {quiz_position: 1};
     }
+    showNextQuestion(){
+              this.setState((state)=>{
+                  return {quiz_position: state.quiz_position + 1}
+              })
+    }
     render(){
-        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
+        const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return(
-                < div className = "QuizQuestion" >
-                    {
-                        isQuizEnd? <QuizEnd/>:
-                    < QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]}/>
+                <div className = "QuizQuestion" >
+                    {isQuizEnd? <QuizEnd/>:
+                       <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler = {this.showNextQuestion.bind(this)}/>
                     }
                 </div>
         );
