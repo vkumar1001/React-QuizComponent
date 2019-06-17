e8251dab0b2065450b2d60c236740180ce1d9a36
vinay kumar <vkumarnit08@gmail.com>
2019-06-17 12:34:33 +0530

    Removed extra div tag

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,11 +9,9 @@ class Quiz extends Component{
     }
     render(){
         return(
-            <div>
                 <div className ='QuizQueation'>
                     {quizData.quiz_questions[0].instruction_text}
                 </div>
-            </div>
         )
         ;
     }
